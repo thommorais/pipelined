8edc201607df87b406200e4588b4fc1b30f95908
thommorais <thommorais@gmail.com>
2025-12-18 00:05:21 -0300

    “It's the job that's never started as takes longest to finish.” ― J.R.R. Tolkien, The Lord of the Rings

--- packages/ui/src/icon/icon-list.mjs
+++
@@ -1,3 +0,0 @@
-// icon name from lucide-react
-// this list is extracted based on what icons are currently used
-export const lucideIconList = new Set(['share', 'trash', 'power'])
--- packages/ui/src/icon/index.ts
+++
@@ -1,4 +0,0 @@
-export { Icon } from './icon'
-export type { IconName } from './icon-names'
-export { IconSprites } from './icon-sprite'
-export { Journ } from './journ'
